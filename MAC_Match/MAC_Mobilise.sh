64c34e714d93bc176de45a05d3103887fe84b443
AdamWHY2K <68286215+AdamWHY2K@users.noreply.github.com>
2021-06-20 08:59:34 +0100

    Create MAC_Mobilise.sh

---
+++ MAC_Match/MAC_Mobilise.sh
@@ -0,0 +1,23 @@
+sleep 15;
+#Give P4wnP1 time to execute MAC_Match HID script
+P4wnP1_cli usb set --rndis;
+#Once MAC_Match is completed eject USB storage
+sleep 3;
+mount -o loop /usr/local/P4wnP1/ums/flashdrive/2GB_FAT.bin /mnt/;
+#Mount USB partition to P4wnP1 filesystem
+sleep 1;
+if [ -e "/mnt/OPFOR" ];
+#If file created by MAC_Match exists
+then
+rm -f /mnt/OPFOR;
+#Delete file created by MAC_Match so you can instantly use it again on a different target
+umount /mnt;
+#Unmount USB partition from P4wnP1 filesystem
+P4wnP1_cli template deploy -f default_attack
+#Deploy master template "default_attack"
+else
+#If file created by MAC_Match doesn't exist
+umount /mnt;
+P4wnP1_cli template deploy -f home
+#Deploy master template "home"
+fi
